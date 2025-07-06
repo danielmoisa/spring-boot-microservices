8d39e13457b8093ae93673ad465a9ba3357ec0b2
Daniel Moisa <danimoisa93@gmail.com>
2025-07-06 10:48:39 +0300

    feat: add inventory service

---
+++ inventoryservice/src/main/java/com/danielmoisa/inventoryservice/entity/Venue.java
@@ -0,0 +1,30 @@
+package com.danielmoisa.inventoryservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "venue")
+public class Venue {
+    @Id
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "total_capacity")
+    private Long totalCapacity;
+}
---
+++ inventoryservice/src/main/java/com/danielmoisa/inventoryservice/service/InventoryService.java
@@ -0,0 +1,45 @@
+package com.danielmoisa.inventoryservice.service;
+
+import com.danielmoisa.inventoryservice.entity.Event;
+import com.danielmoisa.inventoryservice.entity.Venue;
+import com.danielmoisa.inventoryservice.repository.EventRepository;
+import com.danielmoisa.inventoryservice.repository.VenueRepository;
+import com.danielmoisa.inventoryservice.response.EventInventoryResponse;
+import com.danielmoisa.inventoryservice.response.VenueInventoryResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class InventoryService {
+
+    private final EventRepository eventRepository;
+    private final VenueRepository venueRepository;
+
+    @Autowired
+    public InventoryService(final EventRepository eventRepository, final VenueRepository venueRepository) {
+        this.eventRepository = eventRepository;
+        this.venueRepository = venueRepository;
+    }
+
+    public List<EventInventoryResponse> getAllEvents() {
+        final List<Event> events = eventRepository.findAll();
+
+        return events.stream().map(event -> EventInventoryResponse.builder()
+                .event(event.getName())
+                .capacity(event.getLeftCapacity())
+                .venue(event.getVenue())
+                .build()).collect(Collectors.toList());
+    }
+
+    public VenueInventoryResponse getVenueInformation(Long venueId) {
+        final Venue venue = venueRepository.findById(venueId).orElse(null);
+        return VenueInventoryResponse.builder()
+                .venueId(venue.getId())
+                .venueName(venue.getName())
+                .totalCapacity(venue.getTotalCapacity())
+                .build();
+    }
+}
